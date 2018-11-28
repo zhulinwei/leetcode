abdb827fc7964644167244b87c65e38bc72f5846
Level.Z <1171567018@qq.com>
2018-11-28 22:19:59 +0800

    create 7.reverse_integer.js

---
+++ algorithm/7.reverse_integer.js
@@ -0,0 +1,30 @@
+/*
+ * date: 2018-11-28
+ * author: Level.Z
+ * source: https://leetcode-cn.com/problems/reverse-integer/
+ * 
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example1
+ * ```
+ * Input: 123
+ * Output: 321
+ * ```
+ *
+ * Example2
+ * ```
+ * Input: -123
+ * Output: -321
+ * ```
+ *
+ * Example3
+ * ```
+ * Input: 120
+ * Output: 21
+ * ```
+ * Note:
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
+
+
+const reverse = function() {}
