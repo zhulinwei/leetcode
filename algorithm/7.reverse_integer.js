fe54e84b37e437a55946b91a8e71d694e6932e55
Level.Z <1171567018@qq.com>
2018-11-30 12:59:15 +0800

    Update 7.reverse_integer.js

--- algorithm/7.reverse_integer.js
+++ algorithm/7.reverse_integer.js
@@ -26,5 +26,16 @@
  * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
 
-
-const reverse = function() {}
+/*
+ * 解法：在没辅助堆栈/数组的帮助下弹出和推入数字，我们可以使用数学方法，但需要注意是否有溢出
+ */
+const reverse = function(integer) {
+  let reverse = 0;
+  while(integer != 0) {
+    if (integer < 0 && reverse < Math.pow(-2, 31) / 10) return 0;
+    if (integer > 0 && reverse > (Math.pow(2, 31) -1) / 10) return 0;
+    reverse = reverse * 10 + integer % 10;
+    integer = integer > 0 ? Math.floor(integer / 10) : Math.ceil(integer / 10);
+  }
+  return reverse;
+}
