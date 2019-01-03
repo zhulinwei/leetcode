5ec42b9eb176b8db5dab649cb95d2a4d7aaf0627
Level.Z <1171567018@qq.com>
2019-01-03 13:23:54 +0800

    Update 104.maximum_depth_of_binary_tree.js

--- algorithm/104.maximum_depth_of_binary_tree.js
+++ algorithm/104.maximum_depth_of_binary_tree.js
@@ -24,7 +24,7 @@ function TreeNode(val) {
 }
 
 /*
- * 解法一：DFS深度优先/搜索（递归）
+ * 解法一：DFS深度优先搜索（递归）
  * 分别求左右子树的高度，当前节点的高度就是左右子树中较大的那个+1
  * 开始递推
  * 1.H3 = 1 + max(H9, H20)
@@ -34,7 +34,7 @@ function TreeNode(val) {
  * 4.H20 = 1 + 1 = 2;
  * 5.H3 = 1 + 2 = 3;
  * 时间复杂度：O(n)
- * 空间复杂度：极端情况下退化成链表，递归将会被调用N次（即树的高度），故所需额外空间为N，此时的空间复杂度为O(n)；最好情况下（即树完全平衡），空间复杂度为O(logN)
+ * 空间复杂度：极端情况下退化成链表，递归将会被调用N次（即树的高度），故所需额外空间为N，此时的空间复杂度为O(n)；最好情况下（即树完全平衡），空间复杂度为O(logn)
  */
 const maxDepth1 = function (root) {
   if (!root) return 0;
@@ -45,3 +45,32 @@ const maxDepth1 = function (root) {
   // return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
 
+
+/*
+ * 解法二：BFS广度优先搜索（迭代）
+ * 我们使用队列（queue）装载每个节点和标记节点的深度
+ * 1.queue：[ [H3, 1] ]
+ * 2.queue: [ [H9, 2], [H20, 2] ]
+ * 3.queue: [ [H20, 2] ]
+ * 5.queue: [ [H15, 3], [H7, 3] ]
+ * 6.queue: [ [H7, 3] ]
+ * 7.queue: []
+ * 返回深度3
+ * 时间复杂度为O(n)
+ * 空间复杂度为O(n)
+ * 优点：不会出现DFS版本中二叉树过深导致溢出的情况
+ * 缺点：代码实现比DFS版本复杂
+ */
+const maxDepth2 = function (root) {
+  if (!root) return 0;
+
+  let depth = 0;
+  let queue = [ [ root, 1 ] ];
+  while (queue.length > 0) {
+    let [ currentRoot, currentDepth ] = queue.shift();
+    if (!(currentRoot.left && currentRoot.right)) depth = Math.max(depth, currentDepth);
+    if (currentRoot.left) queue.push([currentRoot.left, currentDepth + 1]);
+    if (currentRoot.right) queue.push([currentRoot.right, currentDepth + 1]);
+  }
+  return depth;
+}
