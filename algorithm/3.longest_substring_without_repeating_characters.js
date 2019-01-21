c3132aac1a0fd147506a714bca909231e3fb19b3
Level.Z <1171567018@qq.com>
2019-01-21 13:01:26 +0800

    Update 3.longest_substring_without_repeating_characters.js

--- algorithm/3.longest_substring_without_repeating_characters.js
+++ algorithm/3.longest_substring_without_repeating_characters.js
@@ -27,40 +27,6 @@
  */
 
 /*
- * 解法一
- */
-const lengthOfLongestSubstring = function (string) {
-  let set = new Set();
-  let maxLength = 0;
-  for (let character of string) {
-    if (set.has(character)) set.clear();
-    set.add(character);
-    maxLength = Math.max(maxLength, set.size);
-  }
-  return maxLength;
-}
- 
-const lengthOfLongestSubstring = function (string) {
-  let set = new Set();
-  let max = 0;
-  let removeIndex = 0;
-  let currentIndex = 0;
-  while (startIndex < string.length) {
-    let currentCharacter = string[currentIndex];
-    // 如果集合中含有当前元素值，则删除集合中当前元素值左边的全部元素（包括它自己）
-    if (set.has(currentCharacter)) {
-      let removeCharacter = string[removeIndex];
-      set.delete(removeCharacter);
-      removeIndex += 1;
-    } else {
-      set.add(currentCharacter);
-      max = Math.max(max, set.size);
-    }
-  }
-  return max;
-}
-
-/*
  * 解法一：滑动窗口
  * 维护一个窗口并逐步向右移动，当窗口右侧遇到重复重复元素时，窗口左侧不断删除元素直至没有重复元素
  * 时间复杂度为O(n)
@@ -82,19 +48,18 @@ const lengthOfLongestSubstring1 = function (string) {
 /*
  * 解法二：滑动窗口优化版
  * 我们使用map维护这个窗口，当窗口的右侧遇到重复元素时，窗口的左侧直接跳过重复元素（而不是选择删除）
+ * 个人倾向于使用解法一，简单易懂
  */
-const lengthOfLongestSubstring = function (string) {
+const lengthOfLongestSubstring2 = function (string) {
   let left = 0;
   let right = 0;
   let length = 0;
   let window = new Map();
-  // for (; right < string.length; right++) {
-  //   if (window.get(string.charAt(right))) {
-  //     left = Math.max(window.get(string.charAt(right)), left);
-  //   } else {
-  //     window.set(string.charAt(right), right + 1);
-  //     length = Math.max(right - left + 1, length);
-  //   }
-  // }
+  while (right < string.length) {
+    if (window.get(string.charAt(right))) left = Math.max(window.get(string.charAt(right)), left);
+    window.set(string.charAt(right), right + 1);
+    length = Math.max(right - left + 1, length); 
+    right += 1;
+  }
   return length;
 }
